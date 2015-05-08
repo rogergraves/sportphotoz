7cacb21f1f0b215ed241ef7be15394613361d583
Michael Kasianov <kasianov.michael@gmail.com>
2015-05-08 09:36:52 +0300

    Now user can't pay for his order twice

--- app/controllers/regular/charges_controller.rb
+++ app/controllers/regular/charges_controller.rb
@@ -6,36 +6,29 @@ class Regular::ChargesController < RegularController
   def create
     load_order
 
+    if @order.empty?
+      redirect_to new_regular_charge_path and return
+    end
+
     customer = Stripe::Customer.create(
         :email => 'example@stripe.com',
         :card  => params[:stripeToken]
     )
 
-    begin
-      Stripe::Charge.create(
-          :customer    => customer.id,
-          :amount      => @order.subtotal_cents,  # Amount in cents
-          :description => 'Some Photos',
-          :currency    => 'usd'
-      )
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to charges_path
-    else
-      # If payment ok and no errors raised
-      close_order
-    end
+    charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @order.subtotal_cents,  # Amount in cents
+        :description => 'Some Photos',
+        :currency    => 'usd'
+    )
 
-    # charge = Stripe::Charge.create(
-    #     :customer    => customer.id,
-    #     :amount      => @order.subtotal_cents,  # Amount in cents
-    #     :description => 'Some Photos',
-    #     :currency    => 'usd'
-    # )
-
-  # rescue Stripe::CardError => e
-  #   flash[:error] = e.message
-  #   redirect_to charges_path
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  else
+    # If payment ok and no errors raised
+    close_order
+    # TODO: send email with link to the order
   end
 
   private
